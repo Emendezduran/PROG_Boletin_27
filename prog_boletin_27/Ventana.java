a14f67a7164e744cb04f4d6d0fcb17f8498582c2
Emili <Emili@192.168.1.103>
2019-05-14 18:38:30 +0200

    final commit

---
+++ prog_boletin_27/Ventana.java
@@ -0,0 +1,11 @@
+
+package prog_boletin_27;
+
+public class Ventana {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.setVisible(true);
+    }
+    
+}
